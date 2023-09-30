dbc15dcd54acea550d95df83f04519fe46ed17ad
hector-hs3 <19895925+hector-hs3@users.noreply.github.com>
2023-09-29 20:31:50 -0400

    add desktop theme v1.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      Footer Place Holder...
+      <div className="content">Footer Place Holder...</div>
     </footer>
-  )
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,19 @@ import Logo from "assets/dominican-order.png";
 export default function Header() {
   return (
     <header>
-      <div className="title">
-        <img alt="logo" src={Logo} height="40"></img>
-        <div>St. Dominic Catholic Church Miami</div>
-      </div>
-      <div className="nav">
-        {NavMenu.map((item) => (
-          <NavLink to={item.path}>{item.label}</NavLink>
-        ))}
+      <div className="content">
+        <div className="title">
+          <img alt="logo" src={Logo} height="40"></img>
+          <div>
+            St. Dominic Miami<br></br>
+            Catholic Church
+          </div>
+        </div>
+        <div className="nav">
+          {NavMenu.map((item) => (
+            <NavLink to={item.path}>{item.label}</NavLink>
+          ))}
+        </div>
       </div>
     </header>
   );
